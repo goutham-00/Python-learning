f314d0045de49975657d67f97bd5fc3143362e36
goutham-00 <gouthamallakunta03@gmail.com>
2026-07-07 16:11:22 +0530

    Converter.py
    Add km to miles converter project

---
+++ Converter.py
@@ -0,0 +1,15 @@
+# KM to Miles Converter
+
+km = 10  # you can change this value
+
+# 1 km = 0.621371 miles
+miles = km * 0.621371
+
+print("KM to Miles Converter")
+print("KM:", km)
+print("Miles:", miles)
+
+# Check if distance is long
+is_long_distance = km > 100
+print("Is it a long distance:")
+print(is_long_distance)
